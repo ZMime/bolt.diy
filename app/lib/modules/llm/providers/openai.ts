85a4dfbcb1dc876885224c2b7c9f6fb0fea2e193
Abhishek Taneja <abhishektaneja09@gmail.com>
2025-01-05 23:09:00 +0530

    Update openai.ts

--- app/lib/modules/llm/providers/openai.ts
+++ app/lib/modules/llm/providers/openai.ts
@@ -13,7 +13,7 @@ export default class OpenAIProvider extends BaseProvider {
   };
 
   staticModels: ModelInfo[] = [
-    { name: 'o1', label: 'o1', provider: 'OpenAI', maxTokenAllowed: 8000 },
+   
     { name: 'gpt-4o', label: 'GPT-4o', provider: 'OpenAI', maxTokenAllowed: 8000 },
     { name: 'gpt-4o-mini', label: 'GPT-4o Mini', provider: 'OpenAI', maxTokenAllowed: 8000 },
     { name: 'gpt-4-turbo', label: 'GPT-4 Turbo', provider: 'OpenAI', maxTokenAllowed: 8000 },
